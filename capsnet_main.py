a82d504ec6a27d647e03682d605788ebc5886342
Daniel Lopez <daniellopez@nevada.unr.edu>
2018-03-13 14:58:41 -0700

    finished_model for capsnet_main

--- capsnet_main.py
+++ capsnet_main.py
@@ -11,8 +11,6 @@ from tensorflow.examples.tutorials.mnist import input_data
 
 epsilon = 1e-9
 init_sigma = 0.1
-batch_size = None
-
 
 def preview_mnist(mnist, n_samples = 5):
     plt.figure(figsize=(n_samples*2, 3))
@@ -23,7 +21,6 @@ def preview_mnist(mnist, n_samples = 5):
         plt.axis("on")
 
     print(mnist.train.labels[:n_samples])
-
     plt.show()
 
 
@@ -41,19 +38,16 @@ def safe_norm(vector, axis=-1, keepdims=False, name=None):
         squared_norm = tf.reduce_sum(tf.square(vector), axis=axis, keepdims=keepdims)
         return tf.sqrt(squared_norm + epsilon)
 
-if __name__ == '__main__':
+def make_model():
     # reset tf (to not have to restart the kernel?)
     tf.reset_default_graph()
 
     np.random.seed(42)
     tf.set_random_seed(42)
 
-    mnist = input_data.read_data_sets("data/mnist/")
-    # preview_mnist(mnist, 5)
-
     ### SET UP THE INPUT LAYER ###
     # 28 * 28 images with a single channel (grayscale)
-    X = tf.placeholder(dtype=tf.float32, shape=[batch_size, 28, 28, 1], name="X")
+    X = tf.placeholder(dtype=tf.float32, shape=[None, 28, 28, 1], name="X")
 
     ### SET UP THE PRIMARY CAPS LAYER ###
     primary_caps_n_map = 32
@@ -141,7 +135,7 @@ if __name__ == '__main__':
     y_argmax = tf.argmax(y_probability, axis=2, name="y_probability")
     print(y_argmax)       # to double check the argmax
     # let's get rid of dimensions that have size 1 (since we have the index of the vector that we like
-    y_pred = tf.squeeze(y_argmax, axis=[1,2], name"y_pred")
+    y_pred = tf.squeeze(y_argmax, axis=[1, 2], name="y_pred")
 
     # placeholder for the labels
     y = tf.placeholder(shape=[None], dtype=tf.int64, name="y")
@@ -149,5 +143,91 @@ if __name__ == '__main__':
     # since y will contain the digit classes (from 0 to 9), to get T_k for every class instance...
     T = tf.one_hot(y, depth=digit_caps_n_caps, name="T")
 
-    
+    digit_caps_output_norm = safe_norm(digit_caps_output, axis=-2, keepdims=True, name="digit_caps_output_norm")
+    m_plus = 0.9
+    m_minus = 0.1
+    lambda_ = 0.5
+
+    # Note: on following lines for the T_k values, m_* is a double which gets upgraded to a tensor.
+    # Therefore, the output of m_plus - digit_caps_output_norm (or the other one) returns a tensor at the end
+    # How it maps it is still to be understood by TensorFlow...
+
+    # t_k == 1
+    class_present_raw = tf.square(tf.maximum(0.0, m_plus - digit_caps_output_norm), name="class_present_raw_error")
+    class_present_error = tf.reshape(class_present_raw, shape=(-1, 10), name="class_present_error")
+    # t_k = 0
+    class_absent_raw = tf.square(tf.maximum(0.0, digit_caps_output_norm - m_minus), name="class_absent_raw_error")
+    class_absent_error = tf.reshape(class_absent_raw, shape=(-1, 10), name="class_absent_error")
+    # L = sum of all errors
+    L = tf.add(T*class_present_error, (1.0 - T) * lambda_ * class_absent_error, name="L")
+    print(L)
+    # final margin loss for each instance
+    margin_loss = tf.reduce_mean(tf.reduce_sum(L, axis=1), name="margin_loss")
+    print(margin_loss)
+
+
+    ### ADDING MASK for RECONSTRUCTION ### TO LATER FEED IT TO THE DECODER ###
+    # tell tensorflow to mask the output labels, based on whether or not the predicted output was true
+    mask_with_labels = tf.placeholder_with_default(False, shape=(), name="mask_with_labels")
+    reconstruction_targets = tf.cond(mask_with_labels,
+                                     lambda: y,
+                                     lambda: y_pred,
+                                     name="reconstruction_targets")
+
+    # let's create the mask
+    reconstruction_mask = tf.one_hot(reconstruction_targets, depth=digit_caps_n_caps, name="reconstruction_mask")
+    print(reconstruction_mask)
+    # let's reshape it so that it can be multiplied (for the masking) against the digit caps output
+    reconstruction_mask_reshaped = tf.reshape(reconstruction_mask, shape=[-1, 1, digit_caps_n_caps, 1, 1], name="reconstruction_mask_reshaped")
+    print(reconstruction_mask_reshaped)
+    # apply the mask
+    digit_caps_output_masked = tf.multiply(digit_caps_output, reconstruction_mask_reshaped, name="digit_caps_output_masked")
+    print(digit_caps_output_masked)
+    # one last reshape to flatten the input
+    decoder_input = tf.reshape(digit_caps_output_masked, shape=[-1, digit_caps_n_caps * digit_caps_n_dims], name="decoder_input")
+
+    ### DECODER ###
+    # two fully connected (ReLU) layers, then one dense (sigmoid) layer
+    n_hidden1 = 512
+    n_hidden2 = 1024
+    n_output = 28 * 28
+
+    with tf.name_scope("decoder"):
+        hidden_1 = tf.layers.dense(decoder_input, n_hidden1, activation=tf.nn.relu,    name="hidden_1")
+        hidden_2 = tf.layers.dense(hidden_1,      n_hidden2, activation=tf.nn.relu,    name="hidden_2")
+        decoder_output = tf.layers.dense(hidden_2, n_output, activation=tf.nn.sigmoid, name="decoder_output")
+
+    # this is the reconstruction loss which is just the squared differences between the image and the truth
+    x_flat = tf.reshape(X, shape=[-1, n_output], name="x_flat")
+    squared_diff = tf.square(x_flat - decoder_output, name="squared_diff")
+    reconstruction_loss = tf.reduce_mean(squared_diff, name="reconstruction_loss")
+    print(reconstruction_loss)
+
+    # make the total loss to minimize
+    # end of the model
+    alpha = 0.0005
+    total_loss = tf.add(margin_loss, alpha * reconstruction_loss, name="total_loss")
+    print(total_loss)
+
+    ### SAVE ACCURACY ### TRAIN ### ADD SAVER
+    num_correct = tf.equal(y, y_pred, name="num_correct")
+    accuracy = tf.reduce_mean(tf.cast(num_correct, tf.float32), name="accuracy")
+    optimizer = tf.train.AdamOptimizer()
+    training_op = optimizer.minimize(total_loss, name="training_op")
+    init = tf.global_variables_initializer()
+    saver = tf.train.Saver
+
+    return total_loss, accuracy, training_op, init, saver
+
+
+if __name__ == '__main__':
+    mnist = input_data.read_data_sets("data/mnist/")
+    # preview_mnist(mnist, 5)
+    total_loss, accuracy, training_op, init, saver = make_model()
+
+    n_epochs = 10
+    batch_size = 50
+    restore_checkpoint = True
+
+
 
